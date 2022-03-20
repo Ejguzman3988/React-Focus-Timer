f9c308fd058dd71067b88325e33fa709798f5478
Eriebrto Guzman <eriberto.guzman@outlook.com>
2022-03-20 02:53:45 -0400

    Creates bare html with proper classes for the FocusTimer Component

--- src/components/FocusTimer/FocusTimer.js
+++ src/components/FocusTimer/FocusTimer.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./FocusTimer.css";
 
 export const FocusTimer = () => {
-  return <div className="focus__container">Focus Timer</div>;
+  return (
+    <div className="focus__container">
+      <div className="focus__timer">
+        <div className="focus__label">Focus:</div>
+        <div>0:0:0</div>
+        <div>
+          <button className="focus__button">+</button>
+        </div>
+      </div>
+      <div className="focus__theme">
+        <div className="focus__label">Theme: </div>
+        <select className="focus__dropdown">
+          <option className="focus__option">something</option>
+        </select>
+      </div>
+      <div className="focus__score">
+        <div className="focus__label">High Score: </div>
+        <div className="focus__score__timer">0:0:0</div>
+        <button className="focus__button">O</button>
+      </div>
+    </div>
+  );
 };
