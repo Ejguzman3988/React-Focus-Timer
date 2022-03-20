0f9204ca6177dcae63d8f835867c1c6fa340b050
Eriebrto Guzman <eriberto.guzman@outlook.com>
2022-03-20 15:09:28 -0400

    Adds css for clocks as well as wrapping elements inside of entire component to make sure they can wrap when window is smaller

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -3,13 +3,13 @@ import "./Clock.css";
 
 const Clock = ({ hours, minutes, seconds, blink }) => {
   return (
-    <>
+    <div className="clock">
       <span>{hours}</span>
       <span className={blink ? null : "clock__hide__colon"}>:</span>
       <span>{minutes}</span>
       <span className={blink ? null : "clock__hide__colon"}>:</span>
       <span>{seconds}</span>
-    </>
+    </div>
   );
 };
 
