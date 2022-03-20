0403e4eb50a49c5a127f1045ed84f30dd38eaea8
Eriebrto Guzman <eriberto.guzman@outlook.com>
2022-03-20 02:57:00 -0400

    Adds comments for needed components

--- src/components/FocusTimer/FocusTimer.js
+++ src/components/FocusTimer/FocusTimer.js
@@ -4,22 +4,28 @@ import "./FocusTimer.css";
 export const FocusTimer = () => {
   return (
     <div className="focus__container">
+      {/* Clock Component */}
       <div className="focus__timer">
         <div className="focus__label">Focus:</div>
         <div>0:0:0</div>
         <div>
+          {/* Button Component */}
           <button className="focus__button">+</button>
         </div>
       </div>
       <div className="focus__theme">
+        {/* Select Component */}
         <div className="focus__label">Theme: </div>
         <select className="focus__dropdown">
+          {/* Option Component */}
           <option className="focus__option">something</option>
         </select>
       </div>
       <div className="focus__score">
         <div className="focus__label">High Score: </div>
+        {/* Clock Component */}
         <div className="focus__score__timer">0:0:0</div>
+        {/* Button Component */}
         <button className="focus__button">O</button>
       </div>
     </div>
